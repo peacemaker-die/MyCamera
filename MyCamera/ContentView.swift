7c259e5461d5d9c5c40dcc2ea664ef5be9ba917c
peacemaker-die <die.peacemaker@gmail.com>
2023-02-20 00:22:45 +0900

    Initial Commit

---
+++ MyCamera/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyCamera
+//
+//  Created by 川岸孝輔 on 2023/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 川岸孝輔 on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
